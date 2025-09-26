cfb58167bdeb910fe69544cf3ea5b9d72fe2de5d
subhashkujhuria5558-cmd <subhashkujhuria5558@gmail.com>
2025-09-27 00:40:09 +0530

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  coins: { type: Number, default: 0 },
+  gameStats: {
+    gamesPlayed: { type: Number, default: 0 },
+    gamesWon: { type: Number, default: 0 },
+    totalWinnings: { type: Number, default: 0 }
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
