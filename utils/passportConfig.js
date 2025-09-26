07deb6c1f7fba36e2a65d85bb544bf43e238048e
subhashkujhuria5558-cmd <subhashkujhuria5558@gmail.com>
2025-09-27 03:36:36 +0530

    Update passportConfig.js

--- utils/passportConfig.js
+++ utils/passportConfig.js
@@ -1,13 +1,13 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("../models/User");
 
-module.exports = function (passport) {
+module.exports = (passport) => {
   passport.use(
     new GoogleStrategy(
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: `${process.env.BACKEND_URL}/auth/google/callback`,
+        callbackURL: process.env.GOOGLE_CALLBACK_URL,
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
